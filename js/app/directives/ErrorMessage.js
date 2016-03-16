38011dce314552a5bc013da789baa4e25fd4ad2a
benser1 <briganenser28@gmail.com>
2016-03-16 07:07:34 -0700

    Done.

--- js/app/directives/ErrorMessage.js
+++ js/app/directives/ErrorMessage.js
@@ -1,9 +1,11 @@
 function ErrorMessage() {
 	return {
-		template: '<div>Change this error message to use templateUrl!</div>'
+		templateUrl: 'js/app/views/error.html'
 	};
 }
 
 angular
 	.module('app')
 	.directive('errorMessage', ErrorMessage);
+
+
