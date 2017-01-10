8006c96b77bae62865bdcb9e5aa06cd6625cf223
Christian Whitesides <whitesidescr@gmail.com>
2017-01-09 17:31:06 -0700

    Done.

--- js/app/directives/ErrorMessage.js
+++ js/app/directives/ErrorMessage.js
@@ -1,9 +1,12 @@
 function ErrorMessage() {
 	return {
-		template: '<div>Change this error message to use templateUrl!</div>'
+		templateUrl: '/js/app/views/error.html'
 	};
 }
 
 angular
 	.module('app')
 	.directive('errorMessage', ErrorMessage);
+
+
+// What is an EADDRINUSE error and how to solve it: https://teamtreehouse.com/community/getting-various-error-listen-eaddrinuse-30005858
